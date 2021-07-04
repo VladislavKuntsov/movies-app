5b5794319c5f0b6753cc9fe273bf6581bc7415ea
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-07-04 16:34:38 +0300

    commit

--- src/components/Switch/switch.js
+++ src/components/Switch/switch.js
@@ -2,53 +2,52 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './switch.css';
 
-export default class Switch  extends Component {
+export default class Switch extends Component {
+  static defaultProps = {
+    onRate: () => {},
+    onSearch: () => {},
+    onSearchChange: () => {},
+  };
 
-    static defaultProps = {
-        onRate: () => {},
-        onSearch: () => {},
-        onSearchChange: () => {},
-    };
+  static propTypes = {
+    onRate: PropTypes.func,
+    onSearch: PropTypes.func,
+    onSearchChange: PropTypes.func,
+    switchSearchRate: PropTypes.bool.isRequired,
+  };
 
-    static propTypes = {
-        onRate: PropTypes.func,
-        onSearch: PropTypes.func,  
-        onSearchChange: PropTypes.func,   
-        switchSearchRate: PropTypes.bool.isRequired,
-    };
+  onSearchChange = (event) => {
+    const newTerm = event.target.value;
 
-    onSearchChange = (event) => {
-        const newTerm = event.target.value;
+    const { onSearchChange } = this.props;
 
-        const {onSearchChange} = this.props;
+    onSearchChange(newTerm);
+  };
 
-        onSearchChange(newTerm);
-    } 
+  render() {
+    const { onRate, onSearch, switchSearchRate } = this.props;
 
-    render() {
-        const {onRate, onSearch, switchSearchRate} = this.props
- 
-        const classNamesSearchSpan = !switchSearchRate ? 'active' : null;
-        const classNamesRatedSpan = switchSearchRate ? 'active' : null;
+    const classNamesSearchSpan = !switchSearchRate ? 'active' : null;
+    const classNamesRatedSpan = switchSearchRate ? 'active' : null;
 
-        const classNameSearch = switchSearchRate ? 'sort-panel_search' : 'sort-panel_search active-border';
-        const classNameRated = !switchSearchRate ? 'sort-panel_rated' : 'sort-panel_rated active-border';
+    const classNameSearch = switchSearchRate ? 'sort-panel_search' : 'sort-panel_search active-border';
+    const classNameRated = !switchSearchRate ? 'sort-panel_rated' : 'sort-panel_rated active-border';
 
-        return (
-            <div className='container-sorting'>
-                <div className='sort-panel'>
-                    <div className={classNameSearch}>
-                        <p onClick={onSearch} onKeyPress={onSearch} role="presentation">
-                            <span className={classNamesSearchSpan}>Search</span>
-                        </p>
-                    </div>
-                    <div className={classNameRated}>
-                        <p onClick={onRate} onKeyPress={onSearch} role="presentation">
-                            <span className={classNamesRatedSpan}>Rated</span>
-                        </p>
-                    </div>
-                </div>      
-            </div>  
-        )
-    }
+    return (
+      <div className="container-sorting">
+        <div className="sort-panel">
+          <div className={classNameSearch}>
+            <p onClick={onSearch} onKeyPress={onSearch} role="presentation">
+              <span className={classNamesSearchSpan}>Search</span>
+            </p>
+          </div>
+          <div className={classNameRated}>
+            <p onClick={onRate} onKeyPress={onSearch} role="presentation">
+              <span className={classNamesRatedSpan}>Rated</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
